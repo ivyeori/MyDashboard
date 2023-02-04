724e0e8fa8542fdae38c6ed09f0afb8b64e5157a
Ivyeori <mail@getalex.info>
2023-02-04 19:09:07 +0100

    i just want to be done

--- panel/index.php
+++ panel/index.php
@@ -264,7 +264,7 @@ require "../login/protect.php";
   <div class="col ">  <div class="card info-card revenue-card">
 
 <div class="filter">
-  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="ri-menu-5-line"></i></a>
   <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
     <li class="dropdown-header text-start">
       <h6>Optionen</h6>
@@ -315,7 +315,7 @@ require "../login/protect.php";
   <div class="col ">  <div class="card info-card customers-card">
 
 <div class="filter">
-  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="ri-menu-5-line"></i></a>
   <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
     <li class="dropdown-header text-start">
       <h6>Optionen</h6>
@@ -346,7 +346,7 @@ require "../login/protect.php";
   <div class="col ">  <div class="card info-card customers-card">
 
 <div class="filter">
-  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="ri-menu-5-line"></i></a>
   <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
     <li class="dropdown-header text-start">
       <h6>Optionen</h6>
@@ -618,18 +618,8 @@ require "../login/protect.php";
     <div class="card">
             <div class="card-body">
               <h5 class="card-title">News &amp; Updates</h5>
-              <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div>
+              
+              
 
             <div class="card-body pb-0">
             
